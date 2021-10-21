85e9474265489143ddfed7e4ef66c5ac5362a22e
Artem <a.marinskih1@gmail.com>
2021-10-21 10:03:47 +0500

    added TheAverageOfThePicture

--- TheAverageOfThePicture.js
+++ TheAverageOfThePicture.js
@@ -1,7 +1,21 @@
+const fs = require("fs");
+const {getAverageColor} = require("fast-average-color-node");
+
 module.exports = function picture() {
-//  console.log(process.env)
- const image = process.env.PIC
- if (process.env.PIC === undefined) console.log('error')
- if (process.env.PIC === '') console.log('error')
- console.log(process.env.PIC)
-}
+  const image = process.env.PIC;
+
+  if (!image) {
+    console.log("Путь к картинке не задан");
+    return;
+  }
+
+  fs.readFile(image, (err, data) => {
+    if (err) {
+      console.log("По такому пути нет картинки");
+    }
+
+    getAverageColor(image).then(color => {
+      console.log(color);
+    });
+  });
+};
